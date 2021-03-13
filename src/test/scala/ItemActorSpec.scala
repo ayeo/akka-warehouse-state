e3781917b52d13290f1bbf98ac1cf83c9ef7cba7
ayeo <me@ayeo.pl>
2021-03-13 23:26:46 +0100

    This was the most stupid bug in the world, fixed in the smartest way ever

--- src/test/scala/ItemActorSpec.scala
+++ src/test/scala/ItemActorSpec.scala
@@ -1,55 +1,44 @@
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
-import akka.cluster.typed.Cluster
-import akka.persistence.testkit.PersistenceTestKitPlugin
+import akka.cluster.typed.{Cluster, Join}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.wordspec.AnyWordSpecLike
 import akka.persistence.testkit.scaladsl.{EventSourcedBehaviorTestKit, PersistenceTestKit}
-import pl.ayeo.warehouse.ItemActor.{AddStock, StockUpdated}
+import pl.ayeo.warehouse.ItemActor.AddStock
 import pl.ayeo.warehouse.WarehouseActor.RegisterLocation
 import pl.ayeo.warehouse.{ItemActor, WarehouseActor}
 
 class ItemActorSpec
-  extends ScalaTestWithActorTestKit(PersistenceTestKitPlugin.config.withFallback(
+  extends ScalaTestWithActorTestKit(EventSourcedBehaviorTestKit.config.withFallback(
     ConfigFactory.parseString(
       """
-        |{
-        | akka: {
-        |   actor: {
-        |     provider: cluster,
-        |     allow-java-serialization:true
-        |    }
-        |  }
-        |}
+        | akka.actor.provider = "cluster"
       """.stripMargin
   )))
     with AnyWordSpecLike
     with BeforeAndAfterEach {
 
-  implicit val system2 = ActorSystem(Behaviors.empty, "ClusterSystem") //todo: get rid of name here
-  //val cluster = Cluster(testKit.system)
-  implicit val sharding = ClusterSharding(system2)
+  val cluster = Cluster(testKit.system)
+  cluster.manager ! Join(cluster.selfMember.address)
+  implicit val sharding = ClusterSharding(testKit.system)
   WarehouseActor.init
   ItemActor.init
 
-  val persistenceTestKit = PersistenceTestKit(system)
+  val persistenceTestKit = PersistenceTestKit(testKit.system)
 
   override def beforeEach(): Unit = {
     persistenceTestKit.clearAll()
   }
 
-
   "Item" must {
-    "test if work" in {
+    "reject adding stock to unknown location" in {
       val probe = testKit.createTestProbe[ItemActor.Event]()
       ItemActor.entityRef("23", "100-10") ! AddStock("LC-10", 10, probe.ref)
       probe.expectMessage(ItemActor.InvalidLocation("LC-10"))
     }
 
-    "test if work 2" in {
+    "update stock on valid location" in {
       WarehouseActor.entityRef("23") ! RegisterLocation("LC-10", testKit.system.ignoreRef)
 
       val probe = testKit.createTestProbe[ItemActor.Event]()
